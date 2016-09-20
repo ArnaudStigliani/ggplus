ea7b0da978a7427b5b14720f908aa5109766a4ad
Benjamin <guiastrennec@gmail.com>
2016-09-20 10:10:43 +0200

    Updated README

--- R/theme_readable.R
+++ R/theme_readable.R
@@ -1,11 +1,11 @@
 #' ggplot2 theme
 #'
 #' @description Theme intended to make ggplot2 more readable when used 
-#' in presentation or papers. Background and grid lines were dimed to focus 
-#' the attention on the data itself.
+#' in presentation or papers. Background and major grid lines were dimed 
+#' and minor grid lines removed to focus the attention on the data.
 #'
 #' @param base_size base font size
-#' @param legend.position the position of legends ("none", "left", "right", "bottom", 
+#' @param legend_position position of the legend ("none", "left", "right", "bottom", 
 #' "top", or two-element numeric vector)
 #' 
 #' @seealso \code{\link{ggtheme}}, \code{\link{theme}}
@@ -13,12 +13,12 @@
 #' \dontrun{
 #' ggplot(diamonds, aes(x = price, y = carat, color = cut)) + 
 #'  geom_point(alpha = 0.5) + 
-#'  theme_readable(legend.position = 'top')
+#'  theme_readable(legend_position = 'top')
 #' }
 #' @import ggplot2
 #' @export
 #'
-theme_readable <- function(base_size = 12, legend.position = 'right'){
+theme_readable <- function(base_size = 12, legend_position = 'right'){
   theme(
     # Text size
     text = element_text(size = base_size),
@@ -36,10 +36,10 @@ theme_readable <- function(base_size = 12, legend.position = 'right'){
     axis.title = element_text(face = 'bold'),
     
     # Axis labels
-    axis.text = element_text(size = rel(1.1), face = 'bold', color = 'black'),
+    axis.text = element_text(size = rel(1.1), color = 'black'),
     
     # Legend
-    legend.position = legend.position,
+    legend.position = legend_position,
     legend.key = element_blank(),
     legend.background = element_blank(),
 
@@ -50,7 +50,7 @@ theme_readable <- function(base_size = 12, legend.position = 'right'){
     plot.margin = grid::unit(c(0.01,0.01,0.01,0.01),'npc'),
     
     # Minor grid
-    panel.grid.minor = element_line(color = 'grey88', size = 0.1),
+    panel.grid.minor = element_blank(),
     
     # Major grid 
     panel.grid.major = element_line(color = 'grey88', size = 0.25)
